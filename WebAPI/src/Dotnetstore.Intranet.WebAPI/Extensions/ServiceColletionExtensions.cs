082d32f9cafd3fdb8bbb898d52e9ca764f2eb505
Hans Sjödin <hasse29@hotmail.com>
2024-09-04 13:31:19 +0200

    Added BackgroundService

---
+++ WebAPI/src/Dotnetstore.Intranet.WebAPI/Extensions/ServiceColletionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Dotnetstore.Intranet.WebAPI.Services;
+
+namespace Dotnetstore.Intranet.WebAPI.Extensions;
+
+internal static class ServiceColletionExtensions
+{
+    internal static void AddWebAPI(this IServiceCollection services)
+    {
+        services
+            .AddHostedService<WorkerService>();
+    }
+}
---
+++ WebAPI/src/Dotnetstore.Intranet.WebAPI/Services/WorkerService.cs
@@ -0,0 +1,19 @@
+﻿namespace Dotnetstore.Intranet.WebAPI.Services;
+
+internal sealed class WorkerService(IConfiguration configuration) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // if (logger.IsEnabled(LogLevel.Information))
+            // {
+            //     logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            // }
+            
+            var delay = configuration.GetValue<TimeSpan>("WorkerDelay");
+            
+            await Task.Delay(delay, stoppingToken);
+        }
+    }
+}
