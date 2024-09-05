bbd08a8f0ba6b15ca4286ed8c8b456dc455de66f
Hans Sjödin <hasse29@hotmail.com>
2024-09-05 08:19:56 +0200

    Added entity framework and configurations

---
+++ WebAPI/src/Dotnetstore.Intranet.WebAPI.Utility/Entities/IdentityConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dotnetstore.Intranet.WebAPI.Utility.Entities;
+
+public abstract class IdentityConfiguration<T> : PersonConfiguration<T> where T : Identity
+{
+    public override void Configure(EntityTypeBuilder<T> builder)
+    {
+        base.Configure(builder);
+        
+        builder
+            .Property(x => x.Username)
+            .HasMaxLength(Constants.DefaultUsernameLength)
+            .IsRequired()
+            .IsUnicode(false);
+        
+        builder
+            .Property(x => x.Password)
+            .HasMaxLength(Constants.DefaultPasswordLength)
+            .IsRequired()
+            .IsUnicode(false);
+        
+        builder
+            .Property(x => x.Salt1)
+            .HasMaxLength(Constants.DefaultSaltLength)
+            .IsRequired()
+            .IsUnicode(false);
+        
+        builder
+            .Property(x => x.Salt2)
+            .HasMaxLength(Constants.DefaultSaltLength)
+            .IsRequired()
+            .IsUnicode(false);
+        
+        builder
+            .Property(x => x.Salt3)
+            .HasMaxLength(Constants.DefaultSaltLength)
+            .IsRequired()
+            .IsUnicode(false);
+        
+        builder
+            .Property(x => x.Salt4)
+            .HasMaxLength(Constants.DefaultSaltLength)
+            .IsRequired()
+            .IsUnicode(false);
+
+        builder
+            .Property(x => x.IsBlocked)
+            .IsRequired();
+
+        builder
+            .Property(x => x.BlockedDate)
+            .IsRequired(false);
+    }
+}
