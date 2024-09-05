f60675e842f3d4bcd7a3ada931a0877d29738f6c
Hans Sjödin <hasse29@hotmail.com>
2024-09-05 07:41:33 +0200

    Added BaseAuditableEntity

---
+++ WebAPI/src/Dotnetstore.Intranet.WebAPI.Utility/Entities/IBaseAuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Dotnetstore.Intranet.WebAPI.Utility.Entities;
+
+public interface IBaseAuditableEntity
+{
+    Guid? LastUpdatedBy { get; }
+    
+    DateTimeOffset? LastUpdatedDate { get; }
+    
+    bool IsDeleted { get; }
+    
+    Guid? DeletedBy { get; }
+    
+    DateTimeOffset? DeletedDate { get; }
+    
+    bool IsSystem { get; }
+    
+    bool IsGdpr { get; }
+}
